f07c19ddbbdc4e9bdd90e9582645b5f88cc927ed
Dmitry Zharikov <zdimon77@gmail.com>
2020-08-14 20:41:12 +0300

    commit from zdimon77@gmail.com ----

--- cardgame/app.js
+++ cardgame/app.js
@@ -1,6 +1,6 @@
 var faces = ["clubs","diams","hearts","spades"];
 
-var ranks = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+var ranks = ['Z','A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
 
 var deck = [];
 
@@ -22,7 +22,7 @@ function showCard(card) {
     cardTpl = `<div style="
     width: 69px; height: 94px; 
     background-image: url(images/cards.png);
-    background-position: ${card.rank_id*69}px ${card.face_id*94}px;
+    background-position: -${card.rank_id*69}px ${card.face_id*94}px;
     ">${card.rank} </div>`;
     $('#deck').append(cardTpl);
 }
