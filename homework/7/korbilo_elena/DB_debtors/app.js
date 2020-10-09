8236a118a43bbd6a005a8e091076d82a68f11ed1
Elena Korbilo <olena.korbilo@gmail.com>
2020-10-09 16:45:39 +0300

    hw db_debtors

---
+++ homework/7/korbilo_elena/DB_debtors/app.js
@@ -0,0 +1,12 @@
+var db = {
+  id: Number,
+  name: String,
+  phone: String,
+  sum: Number,
+  isPaid: Boolean,
+
+  init_table: function() {
+    //var container = document.getElementById('container');
+    $(#container).append($(`<table id="table"><tr><td>id</td><td>name</td><td>phone</td><td>sum</td><td>isPaid</td></tr></table>`));
+  }
+}
