4b0b0936d819c814200b9b63cd10b1bb8d0fa064
Pelu_yy04 <mpelyshenko@i.ua>
2020-10-16 20:46:48 +0300

    pz

--- chess/src/server/lib/Board.ts
+++ chess/src/server/lib/Board.ts
@@ -1,21 +1,30 @@
-export class Board {
-    constructor(){
+import { config } from './../config';
 
+export class Board 
+{
+    file= [];
+
+    constructor()
+    {
+        for ( let i=0; i<20; i++)
+        {
+            for ( let j=0; j<20; j++)
+            {
+                this.file.push
+                ({
+                    x: i,
+                    y: j,
+                    color: 'red', 
+                    borderColor: 'green', 
+                    border: 1, 
+                    power: 50,
+                    actor: {}
+                })
+            }
+        }
     }
-
-    getBoard() {
-        return[
-            [
-                {  
-                   x: 1, 
-                   y: 2, 
-                   color: 'red', 
-                   borderColor: 'green', 
-                   border: 1, 
-                   power: 50,
-                   actor: {}
-                }
-            ]
-        ]
+    getBoard() 
+    { 
+        return this.file; 
     }
 }
