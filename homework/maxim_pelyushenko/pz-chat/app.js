0f83982b9318ee69badccb79e0198e64c3efb994
Pelu_yy04 <mpelyshenko@i.ua>
2020-10-29 19:04:58 +0200

    'pz'

---
+++ homework/maxim_pelyushenko/pz-chat/app.js
@@ -0,0 +1,35 @@
+(function( $ )
+{
+    $.fn.quizPlugin = function()
+    {
+        console.log('Myplugin');
+
+        var app = {};
+
+        app.start = function()
+        {
+            console.log('Start');
+
+            if(sessionStorage.getItem('username'))
+            {
+                this.initRoom();
+            }
+            else
+            {
+                this.loginForm()
+            }
+        }
+
+        app.initRoom = function()
+        {
+            console.log('initRoom');
+        }
+
+        app.loginForm= function()
+        {
+            console.log('loginForm');
+        }
+
+        return app;
+    }
+})(jQuery);
