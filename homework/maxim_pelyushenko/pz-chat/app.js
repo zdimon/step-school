33286801d14d759daa5cc8e3adb1830dce9584ff
Pelu_yy04 <mpelyshenko@i.ua>
2020-10-29 20:42:59 +0200

    pzz

--- homework/maxim_pelyushenko/pz-chat/app.js
+++ homework/maxim_pelyushenko/pz-chat/app.js
@@ -1,42 +1,62 @@
-(function( $ )
-{
-    $.fn.quizPlugin = function()
-    {
-        console.log('Myplugin');
+(function( $ ) {
+    $.fn.quizPlugin = function() {
+    
+       console.log('Myplugin')
 
-        var app = {};
+       var app = {};
 
-        app.start = function()
-        {
-            console.log('Start');
+       app.serverUrl = 'localhost:7777';
 
-            if(sessionStorage.getItem('username'))
-            {
+       app.start = function() {
+           console.log('Start app');
+
+            if(sessionStorage.getItem('username')) {
                 this.initRoom();
+            } else {
+                this.loginForm();
+                this.getStickers();
             }
-            else
-            {
-                this.loginForm()
-            }
-        }
 
-        app.initRoom = function()
-        {
-            console.log('initRoom');
-        }
+       }
 
-        app.loginForm = function() 
-        {
-            console.log('loginForm');
-            let tpl = `<div class="tab-pane fade active show" id="default-block" role="tabpanel">
+       app.initRoom = function() {
+        console.log('initRoom');
+       };
+
+       app.getStickers = function() {
+           $.get(`http://${this.serverUrl}/v1/quiz/sticker/list`,(data) => {
+                console.log(data);
+                data.forEach((el) => {
+                    let tpl = `<img width="50" src="${el.get_url}" />`
+                    $('#Stickers').append(tpl);
+                });
+           })
+       }
+
+       app.loginForm = function() {
+        console.log('loginForm');
+        let tpl = `<div class="tab-pane fade active show" id="default-block" role="tabpanel">
             <div class="chat-start">
-               <input type="text" />
-               <button id="chat-start" class="btn bg-white mt-3">Start
-               Conversation!</button>
-            </div>`;
-            $('#loginForm').append(tpl);
-        }
+            <input id="userName" type="text" />
+            <div id="Stickers"></div>
+            <button id="chat-start" class="btn bg-white mt-3">Start
+            Conversation!</button>
+            </div>
+        </div>`;
+        $('#loginForm').append(tpl);
+        console.log($('#loginForm').find('#chat-start'));
+        console.log(this.doLogin);
+        $('#loginForm').find('#chat-start').on('click', this.doLogin)
+       }
 
-        return app;
-    }
+       app.doLogin = function() {
+ 
+           sessionStorage.setItem('username',$('#userName').val())
+       }
+
+
+          
+       return app;
+
+    };
 })(jQuery);
