3aae59a45d65fea16faff81b3b5635f8bb2a930a
Dmitry Zharikov <zdimon77@gmail.com>
2020-10-29 20:05:48 +0200

    commit from zdimon77@gmail.com ----

--- quiz/work/app.js
+++ quiz/work/app.js
@@ -5,13 +5,16 @@
 
        var app = {};
 
+       app.serverUrl = 'localhost:7777';
+
        app.start = function() {
            console.log('Start app');
 
             if(sessionStorage.getItem('username')) {
                 this.initRoom();
             } else {
-                this.loginForm()
+                this.loginForm();
+                this.getStickers();
             }
 
        }
@@ -20,15 +23,35 @@
         console.log('initRoom');
        };
 
+       app.getStickers = function() {
+           $.get(`http://${this.serverUrl}/v1/quiz/sticker/list`,(data) => {
+                console.log(data);
+                data.forEach((el) => {
+                    let tpl = `<img width="50" src="${el.get_url}" />`
+                    $('#Stickers').append(tpl);
+                });
+           })
+       }
+
        app.loginForm = function() {
         console.log('loginForm');
         let tpl = `<div class="tab-pane fade active show" id="default-block" role="tabpanel">
-        <div class="chat-start">
-           <input type="text" />
-           <button id="chat-start" class="btn bg-white mt-3">Start
-           Conversation!</button>
+            <div class="chat-start">
+            <input id="userName" type="text" />
+            <div id="Stickers"></div>
+            <button id="chat-start" class="btn bg-white mt-3">Start
+            Conversation!</button>
+            </div>
         </div>`;
         $('#loginForm').append(tpl);
+        console.log($('#loginForm').find('#chat-start'));
+        console.log(this.doLogin);
+        $('#loginForm').find('#chat-start').on('click', this.doLogin)
+       }
+
+       app.doLogin = function() {
+ 
+           sessionStorage.setItem('username',$('#userName').val())
        }
 
 
