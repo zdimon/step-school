059512a6f1955d0e3ee14600586c05d7448afa1b
Dmitry Zharikov <zdimon77@gmail.com>
2020-11-23 20:34:07 +0200

    commit from zdimon77@gmail.com ----

--- oop/app2.js
+++ oop/app2.js
@@ -13,14 +13,25 @@ class Bankomat{
     }
 
     check(number, pin) {
-
+        let isCorrect = false;
+        this.cards.forEach((card) => {
+            console.log(card.number,number);
+            if(card.number === number){
+                console.log(card.pin,pin);
+                if(card.pin === pin){
+                    isCorrect = true;
+                }
+            }
+        });
+        return isCorrect;
     }
 
     replanish() {
+        console.log('replanish');
         let number = $('#cards').val();
         let pin = $('#pin').val();
         let sum = $('#sum').val();
-        if (this.check()) {
+        if (this.check(number,pin)) {
             console.log('Werry welll');
         } else {
             alert('Pin error!!!');
@@ -47,15 +58,16 @@ class Bankomat{
             </p>
         <div/>
         `;
-
-        $('#root').append(tpl);        
+       
+        $('#root').append(tpl);     
+        $('#r-button').on('click',() => {this.replanish()});    
     }
 
 }
 
-const card1 = new Card(123456,123,0);
-const card2 = new Card(223456,223,0);
-const card3 = new Card(323456,323,0);
+const card1 = new Card('123456','123',0);
+const card2 = new Card('223456','223',0);
+const card3 = new Card('323456','323',0);
 
 const baankomat = new Bankomat([card1,card2,card2]);
 baankomat.display();
