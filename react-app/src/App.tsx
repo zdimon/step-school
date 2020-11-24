2eaf9b101c4cc4519b14c70c1ec6d483cbe355e7
Dmitry Zharikov <zdimon77@gmail.com>
2020-11-24 20:15:28 +0200

    commit from zdimon77@gmail.com ----

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { Post } from './Post';
 import { Comment } from './Comment';
-
+import {CommentForm} from './CommentForm'
 
 
 function App() {
-  const comments = ['One', 'Two', 'Three'];
+  const users = [
+    {name: 'Dima', image: '1.png', post: 'Hello!!!'},
+    {name: 'Dima2', image: '1.png', post: 'Hello2!!!'},
+    {name: 'Dima3', image: '1.png', post: 'Hello3!!!'}
+  ];
   return (
     <div className="App">
       <Post />
-      { comments.map((el,index) => <Comment comment={el} key={index} />) }
+      { users.map((el,index) => <Comment user={el} key={index} />) }
+      <CommentForm />
     </div>
   );
 }
