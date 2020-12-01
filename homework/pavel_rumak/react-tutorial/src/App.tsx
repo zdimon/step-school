2df5cc6fe348e60604c4ce5aad9a0bb4a16eecf5
pavel_rumak <pavel_rumak@ukr.net>
2020-12-01 20:38:02 +0200

    -=-

--- homework/pavel_rumak/react-tutorial/src/App.tsx
+++ homework/pavel_rumak/react-tutorial/src/App.tsx
@@ -1,22 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import {Article} from './components/Article/Article'
+import React,{MutableRefObject, useRef} from 'react';
+//import {Article} from './components/Article/Article'
 
 import './App.css';
 
-function App() {
- const [visible, setVisible] = useState<boolean>(false)
-  useEffect(() => {
-    console.log('render')
-    return
-  }, [])
 
+
+/*const arr1 = arr.map((item, i, arr)=> {
+  console.log(item,i,arr)
+  return item * 2
+
+
+  const arr1 = arr.map((item, i, arr)=> {
+  const obj = {name: 'Alex'}
+  return {...obj,age:item*2}
+})*/
+
+
+
+
+
+function App() {
+
+  const emailRef = useRef({ value: ''})
+
+ /* const handleSubmit= (e:any)=>{
+    e.preventDefault()
+    const email = e.target.elements[0].value
+    const password = e.target.elements[1].value
+    console.log(email, password)
+  }*/
+      /*<form onSubmit={handleSubmit}>
+        <input type="email"></input>
+        <input type="password"></input>
+        <button type="submit">Submit</button>
+      </form>*/
+
+
+      interface formState {
+        email: string
+        password: string
+      }
+
+      const [formstate, setFormState] = React.useState<formState>({
+        email:'',
+        password: ''
+      })
+      const handleChange = (key: string)=> (e: any)=>{
+        const {target :{value}}=e
+        setFormState({
+          ...formstate,
+          [key]:value
+        } as formState)
+       
+      }
+      const handleSubmit= (e:any)=>{
+        e.preventDefault()
+        console.log('FormState', emailRef.current.value)
+      }
   return (
     <div className="App">
-      <button onClick={()=>setVisible(prevState => !prevState)}>Trigger</button>
-      {visible && <Article
-      title = {'Post 1'}
-      date={new Date().toString()}
-      />}
+      <form onSubmit={handleSubmit}>
+        <input ref={emailRef as MutableRefObject<any>}  type="email" onChange={handleChange('email')} />
+        <input type="password" onChange={handleChange('password')} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
