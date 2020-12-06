f2f976fbae1c3ab42e84d7dc300323ebd52023f3
Denchik23083 <deniskudravov228@gmail.com>
2020-12-06 20:48:52 +0200

    just

---
+++ homework/2/DZ_Kudryavov_Denis/DZ_3/code.js
@@ -0,0 +1,12 @@
+//1
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM готов!");
+});
+
+//2
+window.onload = function() {
+    console.log('Pictute download');
+}
+
+//3
+
